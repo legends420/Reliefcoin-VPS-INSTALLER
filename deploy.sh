097dabe16a4fe4f9ba45e9638e842713643baf49
legends420 <highlifesmokes@gmail.com>
2018-11-25 21:39:57 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -170,7 +170,7 @@ function install_prerequisites()
     #pre-reqs for running the daemon file
     sudo apt update > /dev/null 2>&1
     echo -ne "${YELLOW} [###               ]\r"
-    sudo apt install -y pwgen libwww-perl build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils libdb++-dev libminiupnpc-dev libboost-all-dev libqrencode-dev unzip > /dev/null 2>&1
+    sudo apt install -y pwgen libwww-perl build-essential libtool automake autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils libdb++-dev libminiupnpc-dev libboost-all-dev libqrencode-dev unzip libzmq3-dev > /dev/null 2>&1
     echo -ne " [########          ]\r"
     sudo add-apt-repository -y ppa:bitcoin/bitcoin > /dev/null 2>&1
     echo -ne " [##############    ] \r"
